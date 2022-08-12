ec094261a22d0990792cf71ad5aa9fadfd6ebcfc
De <spark49942@gmail.com>
2022-08-12 21:04:07 +0300

    Z3

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ for (int bar = 0; bar < bnum; bar++)
 {
     for (int col = 0; col < cnum; col++) 
     {
-        if (col % 2 != 0) { Console.Write($"{bar + col}" + "(" + $"{Math.Pow(bar + col, 2)}" + ")" + "\t"); }
+        if (col % 2 != 0 && bar % 2 != 0) { Console.Write($"{bar + col}" + "(" + $"{Math.Pow(bar + col, 2)}" + ")" + "\t"); }
         else Console.Write($"{bar + col}" + "\t");
     }
     Console.WriteLine("");
@@ -33,9 +33,30 @@ for (int bar = 0; bar < bnum; bar++)
 
 void Z3()
 {
-
+Console.WriteLine("51. Введите значения ширины и длины: ");
+int bnum = Convert.ToInt32(Console.ReadLine());
+int cnum = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int [,] table = new int[bnum, cnum];
+Console.WriteLine("В массиве: ");
+for (int bar = 0; bar < bnum; bar++) 
+{
+    for (int col = 0; col < cnum; col++) 
+    {
+        Console.Write($"{bar + col}" + "\t");
+    }
+}
+for (int bar = 0; bar < bnum; bar++) 
+{
+    for (int col = 0; col < cnum; col++) 
+    {
+        sum += col;
+    }
+}
+Console.WriteLine("Сумма главной диагонали равна " + sum);
+Console.WriteLine("");
 }
 
 //Z1();
-Z2();
+//Z2();
 Z3();
