ddfe6d8e6a41947d0d89be1b66a103101cb5d9f9
De <spark49942@gmail.com>
2022-08-12 20:28:07 +0300

    Z1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿void Z1()
 {
-Console.WriteLine("3. ");
-int [,] table = new int[3, 4];
-for (int bar = 0; bar < 3; bar++) 
+Console.WriteLine("48. Введите значения ширины и длинны: ");
+int bnum = Convert.ToInt32(Console.ReadLine()), 
+    cnum = Convert.ToInt32(Console.ReadLine());
+int [,] table = new int[bnum, cnum];
+for (int bar = 0; bar < bnum; bar++) 
 {
-    for (int col = 0; col < 4; col++) 
+    for (int col = 0; col < cnum; col++) 
     {
         Console.Write($"{bar + col}");
     }
