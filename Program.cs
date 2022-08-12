e415793b2dc31cbcd9037d286aa470d769dc0243
De <spark49942@gmail.com>
2022-08-12 20:34:10 +0300

    Z2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿void Z1()
 {
-Console.WriteLine("48. Введите значения ширины и длинны: ");
+Console.WriteLine("48. Введите значения ширины и длины: ");
 int bnum = Convert.ToInt32(Console.ReadLine()), 
     cnum = Convert.ToInt32(Console.ReadLine());
 int [,] table = new int[bnum, cnum];
@@ -16,7 +16,19 @@ for (int bar = 0; bar < bnum; bar++)
 
 void Z2()
 {
-
+Console.WriteLine("48. Введите значения ширины и длины: ");
+int bnum = Convert.ToInt32(Console.ReadLine()), 
+    cnum = Convert.ToInt32(Console.ReadLine());
+int [,] table = new int[bnum, cnum];
+for (int bar = 0; bar < bnum; bar++) 
+{
+    for (int col = 0; col < cnum; col++) 
+    {
+        if (col % 2 == 0) { Console.Write($"{Math.Pow(bar + col, 2)}"); }
+        else Console.Write($"{bar + col}");
+    }
+    Console.WriteLine("");
+}
 }
 
 void Z3()
@@ -24,6 +36,6 @@ void Z3()
 
 }
 
-Z1();
+//Z1();
 Z2();
 Z3();
