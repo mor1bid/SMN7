6e4cfbfb8fd36fb20ab413a759191ef37e2c1a69
De <spark49942@gmail.com>
2022-08-12 20:24:00 +0300

    Z1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 ﻿void Z1()
 {
-
+Console.WriteLine("3. ");
+int [,] table = new int[3, 4];
+for (int bar = 0; bar < 3; bar++) 
+{
+    for (int col = 0; col < 4; col++) 
+    {
+        Console.Write($"{bar + col}");
+    }
+    Console.WriteLine("");
+}
 }
 
 void Z2()
