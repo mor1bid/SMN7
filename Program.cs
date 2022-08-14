7deab024d28e69ad5d5961fe2a28c8a889a56ba6
De <spark49942@gmail.com>
2022-08-15 01:05:58 +0300

    DZ

--- Program.cs
+++ Program.cs
@@ -30,27 +30,26 @@ for (int i = 0, bar = 1; bar<=bnum; bar++)
         }
 }
 Console.WriteLine("52.");
-// for (int i = 0, bar = 0; bar<bnum; bar++) 
-// {
-//     for (int col = 0; col<cnum; i++, col++)
-//     {
-//         int num = ra.Next(-10, 11);
-//         ray[i] = num;
-//         Console.Write(ray[i] + "\t");
-//     }
-//     Console.WriteLine("");
-// }
+for (int i = 0, bar = 0; bar<bnum; bar++) 
+{
+    for (int col = 0; col<cnum; i++, col++)
+    {
+        int num = ra.Next(-10, 11);
+        ray[i] = num;
+        Console.Write(ray[i] + "\t");
+    }
+    Console.WriteLine("");
+}
 Console.Write("Среднее арифметическое каждого столбца: ");
 for (int i = 0, col = 1; col<=cnum; col++, i++) 
 {
     double sum = 0;
     for (int i2 = i, bar = 1; bar<=bnum; bar++)
     {
-        if (i2>0) { sum += ray[i2]; Console.Write("(" + ray[i2] + ", "); }
-        else { sum += ray[i2]; Console.Write("(" + ray[i2] + ", ");}
+        if (i2>0) { sum += ray[i2]; }
+        else { sum += ray[i2]; }
         i2 += cnum;
     }
-    Console.Write(") - ");
     Console.Write($"{sum*bnum}" + ", ");
 }
 Console.Write("\b\b");
