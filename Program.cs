08bf3e780fe80f319211a4c9c6a499b7dfc395ac
De <spark49942@gmail.com>
2022-08-14 16:45:17 +0300

    DZ2-nichegoneponyatno

--- Program.cs
+++ Program.cs
@@ -27,30 +27,31 @@ int cnum = Convert.ToInt32(Console.ReadLine());
 Random ra = new Random();
 int si = bnum*cnum;
 double [] ray = new double[si];
-for (int i=0; i<si; i++) 
-    {
-        double num = Math.Round (ra.NextDouble() * (-10.5-11)+11, 1);
-        ray[i] = num;
-    }
-double [] ar = new double[si];
+// for (int i=0; i<si; i++) 
+//     {
+//         double num = Math.Round (ra.NextDouble() * (-10.5-11)+11, 1);
+//         ray[i] = num;
+//     }
+// double [] ar = new double[si];
 int [,] table = new int[bnum, cnum];
 for (int i = 0, bar = 0; bar < bnum; bar++) 
 {
     for (int col = 0; col<cnum; i++, col++)
     {
-        ar[i] = ray[i];
-        Console.Write(ar[i] + "\t");
+        double num = Math.Round (ra.NextDouble() * (-10.5-11)+11, 1);
+        ray[i] = num;
+        Console.Write(ray[i] + "\t");
     }
     Console.WriteLine("");
 }
 Console.WriteLine("Введите желаемые значения строки и столбца: ");
 int mybar = Convert.ToInt32(Console.ReadLine());
 int mycol = Convert.ToInt32(Console.ReadLine());
-for (int i = 0, bar = 1; bar <= bnum; bar++, i++)
+for (int i = 0, bar = 1; bar <= bnum; bar++)
 {
     if (mybar == bar) 
     {
-        for (int col = 1; col<=cnum; col++)
+        for (int col = 1; col<=cnum; i++, col++)
         {
             if (mycol == col) { Console.WriteLine(ray[i]); break; }
             else if (mycol>cnum || mycol<=0) { Console.WriteLine("Такого элемента нет."); break; }
