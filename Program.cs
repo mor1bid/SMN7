03eb23c1d469528531fc6046674153607509ded5
De <spark49942@gmail.com>
2022-08-14 13:44:45 +0300

    DZ1f

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ for (int bar = 0; bar < bnum; bar++)
     for (int i=0, col = 0; col<cnum; col++, i++) 
     {
         //int numa = ra.Next(-10, 11);
-        double numb = Math.Round (ra.NextDouble() * (-10.5-11)+11, 1);
+        double numb = Math.Round (ra.NextDouble() * (-10-11)+11, 1);
         ray[i] = numb;
         Console.Write(ray[i] + "\t");
         // ray[i] = numa;
