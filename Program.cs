51d5bea4d47876fda814c673240443eaa6d95258
De <spark49942@gmail.com>
2022-08-14 14:08:08 +0300

    DZ2

--- Program.cs
+++ Program.cs
@@ -9,18 +9,52 @@ int [,] table = new int[bnum, cnum];
 double [] ray = new double[si]; 
 for (int bar = 0; bar < bnum; bar++) 
 {
-    for (int i=0, col = 0; col<cnum; col++, i++) 
+    for (int i=0, col = 0; col<cnum; i++, col++) 
     {
-        //int numa = ra.Next(-10, 11);
         double numb = Math.Round (ra.NextDouble() * (-10.5-11)+11, 1);
         ray[i] = numb;
         Console.Write(ray[i] + "\t");
-        // ray[i] = numa;
-        // Console.Write(ray[i] + "\t");
     }
     Console.WriteLine("");
 }
 }
 
+void DZ2()
+{
+Console.WriteLine("48. Введите значения ширины и длины: ");
+int bnum = Convert.ToInt32(Console.ReadLine());
+int cnum = Convert.ToInt32(Console.ReadLine());
+Random ra = new Random();
+int si = ra.Next(bnum*cnum);
+int [,] table = new int[bnum, cnum];
+double [] ray = new double[si]; 
+for (int bar = 0; bar < bnum; bar++) 
+{
+    for (int i=0, col = 0; col<cnum; col++, i++) 
+    {
+        int num = ra.Next(-10, 11);
+        ray[i] = num;
+        Console.Write(ray[i] + "\t");
+    }
+    Console.WriteLine("");
+}
+Console.WriteLine("Введите желаемые значения строки и столбца: ");
+int mybar = Convert.ToInt32(Console.ReadLine());
+int mycol = Convert.ToInt32(Console.ReadLine());
+for (int bar = 0; bar < bnum; bar++)
+{
+    if (bar == mybar) 
+    {
+    for (int i=0, col = 0; col<cnum; col++, i++) 
+    {
+        if (col == mycol)
+        Console.WriteLine(col);
+        else if (mycol>cnum) { Console.WriteLine("Такого элемента нет."); }
+    }
+    }
+    else if (mybar>bnum) { Console.WriteLine("Такого элемента нет."); }
+}
+}
 
-DZ1();
+//DZ1();
+DZ2();
