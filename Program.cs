8f8f909a292182f297813d2443ecf975d633f6fb
De <spark49942@gmail.com>
2022-08-14 13:21:11 +0300

    DZ1

--- Program.cs
+++ Program.cs
@@ -1,65 +1,23 @@
-﻿void Z1()
+﻿void DZ1()
 {
 Console.WriteLine("48. Введите значения ширины и длины: ");
-int bnum = Convert.ToInt32(Console.ReadLine()), 
-    cnum = Convert.ToInt32(Console.ReadLine());
-int [,] table = new int[bnum, cnum];
-for (int bar = 0; bar < bnum; bar++) 
-{
-    for (int col = 0; col < cnum; col++) 
-    {
-        Console.Write($"{bar + col}" + "\t");
-    }
-    Console.WriteLine("");
-}
-}
-
-void Z2()
-{
-Console.WriteLine("49. Введите значения ширины и длины: ");
-int bnum = Convert.ToInt32(Console.ReadLine()), 
-    cnum = Convert.ToInt32(Console.ReadLine());
-int [,] table = new int[bnum, cnum];
-for (int bar = 0; bar < bnum; bar++) 
-{
-    for (int col = 0; col < cnum; col++) 
-    {
-        if (col % 2 != 0 && bar % 2 != 0) { Console.Write($"{bar + col}" + "(" + $"{Math.Pow(bar + col, 2)}" + ")" + "\t"); }
-        else Console.Write($"{bar + col}" + "\t");
-    }
-    Console.WriteLine("");
-}
-}
-
-void Z3()
-{
-Console.WriteLine("51. Введите значения ширины и длины: ");
 int bnum = Convert.ToInt32(Console.ReadLine());
 int cnum = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
+Random ra = new Random();
+int si = ra.Next(2, 11);
 int [,] table = new int[bnum, cnum];
-Console.WriteLine("В массиве: ");
+double [] ray = new double[si]; 
 for (int bar = 0; bar < bnum; bar++) 
 {
-    for (int col = 0; col < cnum; col++) 
+    for (int i=0, col = 0; col<cnum; col++, i++) 
     {
-        Console.Write($"{bar + col}" + "\t");
+        double numb = Math.Round (ra.NextDouble() * (-10.5-11.5)+11.5, 2);
+        ray[i] = numb;
+        Console.WriteLine(ray[i] + "\t");
     }
     Console.WriteLine("");
 }
-    int plus = 1;
-for (int bar = 0; bar < bnum; bar++, plus++) 
-{
-    for (int col = 0; col <= plus; col++) 
-    {
-        sum += col;
-        col += plus;
-    }
-}
-Console.Write(" Сумма главной диагонали равна " + sum);
-Console.WriteLine(" ");
 }
 
-//Z1();
-//Z2();
-Z3();
+
+DZ1();
