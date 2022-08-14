80bd00f35bdf2e45c07e3dc1488e7eb759e44f1a
De <spark49942@gmail.com>
2022-08-14 15:18:19 +0300

    DZ2-4

--- Program.cs
+++ Program.cs
@@ -50,7 +50,7 @@ for (int bar = 1; bar <= bnum; bar++)
     {
     for (int i=0, col = 1; col<=cnum; i++, col++)
     {
-        if (col == mycol) { Console.WriteLine(ray[i]); return; }
+        if (col == mycol) { Console.WriteLine(ray[i]); break; }
         else if (mycol>cnum || mycol<=0) { Console.WriteLine("Такого элемента нет."); break; }
     }
     break;
