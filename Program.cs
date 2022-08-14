27fe329da180cb1f9c5640eb6cfc15e9b2935208
De <spark49942@gmail.com>
2022-08-14 16:39:15 +0300

    DZ2-nichegoneponyatno

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@ for (int bar = 1; bar <= bnum; bar++)
     {
         for (int i=0, col = 1; col<=cnum; i++, col++)
         {
-            if (mycol == col) { Console.WriteLine(ray[i]); break; }
+            if (mycol == col) { Console.WriteLine(ar[i]); break; }
             else if (mycol>cnum || mycol<=0) { Console.WriteLine("Такого элемента нет."); break; }
         }
     break;
