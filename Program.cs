ee7cc10ae472fceec35c7383a0cc3a1935c4d52b
De <spark49942@gmail.com>
2022-08-14 22:46:51 +0300

    DZ3

--- Program.cs
+++ Program.cs
@@ -40,10 +40,10 @@ for (int i = 0, bar = 0; bar<bnum; bar++)
     Console.WriteLine("");
 }
 Console.Write("52. Среднее арифметическое каждого столбца: ");
-for (int i = 0, col = 0; col<cnum; col++) 
+for (int col = 0; col<cnum; col++) 
 {
     double sum = 0;
-    for (int bar = 0; bar<bnum; bar++, i++)
+    for (int i = 0, bar = 0; bar<bnum; bar++, i++)
     {
         Console.Write("(" + ray[i] + ", ");
         sum += ray[i];
